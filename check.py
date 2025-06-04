763bcf8c52d318d2fe7874874ad70067a3331c8d
Vamsee369 <vamseev213@gmail.com>
2025-06-04 10:41:18 +0530

    Add day 1 Python practice files

--- check.py
+++ check.py
@@ -10,30 +10,10 @@
 
 # Create a simple calculator using functions.✅
 
-# Write a program that reads a file and prints its contents.
+# Write a program that reads a file and prints its contents.✅
 
 # Count word frequency in a sentence using a dictionary.✅
 
 # Sort a list of tuples based on the second value.✅
 
-# Create a menu-driven program (like a small app with choices).
-
-# This program reads and prints the contents of a file named "example.txt"
-
-def read_file():
-    try:
-        # Open the file in read mode
-        with open("example.txt", "r") as file:
-            # Read the contents of the file
-            content = file.read()
-
-            # Print the file content
-            print("File Contents:\n")
-            print(content)
-
-    except FileNotFoundError:
-        print("Error: The file 'example.txt' was not found.")
-
-
-# Call the function
-read_file()
+# Create a menu-driven program (like a small app with choices).✅
---
+++ projects/calculator.py
@@ -0,0 +1,38 @@
+def add(x, y):
+    return (x+y)
+
+
+def subtract(x, y):
+    return (x-y)
+
+
+def multiply(x, y):
+    return (x*y)
+
+
+def divison(x, y):
+    if y != 0:
+        return (x/y)
+    else:
+        print("can not be divideed with zero (0)  ")
+
+
+print("choose an option")
+print("1 for adition :")
+print("2 for subtraction :")
+print("3 for multiplication : ")
+print("4 for divison : ")
+choice = input("what is your choice ")
+num1 = float(input("enter first number : "))
+num2 = float(input("enter second number : "))
+if choice == '1':
+    result = add(num1, num2)
+elif choice == '2':
+    result = subtract(num1, num2)
+elif choice == '3':
+    result = multiply(num1, num2)
+elif choice == '4':
+    result = divison(num1, num2)
+else:
+    print("an error occured check once. ")
+print("the result is : ", result)

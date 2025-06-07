5b6fad0d3990bb91844a6dc9291a6f30a4773f8d
Vamsee369 <vamseev213@gmail.com>
2025-06-07 09:24:44 +0530

    day 5 - tuples and sets completed

---
+++ python practice/day 5/sets_demo.py
@@ -0,0 +1,13 @@
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+print("Union:", A | B)
+print("Intersection:", A & B)
+print("Difference:", A - B)
+my_fruits = {"apple", "banana", "cherry"}
+print("Fruits:", my_fruits)
+
+
+my_fruits.add("orange")
+my_fruits.remove("banana")
+print("Updated Fruits:", my_fruits)
